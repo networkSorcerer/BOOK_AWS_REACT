bf3e245e726c680fc1daea50809d909823e8d491
networkSorcerer <gigattfyhig@gmail.com>
2024-11-26 17:27:42 +0900

    ecosystem

--- src/api-config.js
+++ src/api-config.js
@@ -3,10 +3,10 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
-    backendHost = "http://localhost:8080";
+  backendHost = "http://54.180.139.78:4040";
 } else {
-    // 기본값 또는 다른 환경을 위한 설정
-    backendHost = "https://your-production-url.com"; // 필요에 따라 다른 URL로 설정
+  // 기본값 또는 다른 환경을 위한 설정
+  backendHost = "https://your-production-url.com"; // 필요에 따라 다른 URL로 설정
 }
 
 // 템플릿 리터럴에서 `${}` 사용
