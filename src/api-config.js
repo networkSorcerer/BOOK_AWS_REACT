a399bb9cb723a42abcd484d3937fc6cdb1ffdf25
networkSocerer <gigattfyhig@gmail.com>
2024-11-26 19:11:46 +0900

    react

--- src/api-config.js
+++ src/api-config.js
@@ -6,7 +6,7 @@ if (hostname === "localhost") {
   backendHost = "http://54.180.139.78:4040";
 } else {
   // 기본값 또는 다른 환경을 위한 설정
-  backendHost = "https://your-production-url.com"; // 필요에 따라 다른 URL로 설정
+  backendHost = "http://54.180.139.78:4040"; // 필요에 따라 다른 URL로 설정
 }
 
 // 템플릿 리터럴에서 `${}` 사용
