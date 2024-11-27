259e93f1cc89a426b867848c8a991d4f36782b38
networkSocerer <gigattfyhig@gmail.com>
2024-11-27 21:36:47 +0900

    react

--- src/api-config.js
+++ src/api-config.js
@@ -3,10 +3,10 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
-  backendHost = "http://54.180.139.78:4040";
+  backendHost = "http://localhost:8080";
 } else {
   // 기본값 또는 다른 환경을 위한 설정
-  backendHost = "http://54.180.139.78:4040"; // 필요에 따라 다른 URL로 설정
+  backendHost = "http://localhost:8080"; // 필요에 따라 다른 URL로 설정
 }
 
 // 템플릿 리터럴에서 `${}` 사용
--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -36,14 +36,19 @@ export function call(api, method, request) {
 }
 
 export function signin(userDTO) {
-  return call("/auth/signin", "POST", userDTO).then((response) => {
-    if (response.token) {
-      // 로컬 스토리지에 토큰 저장
-      localStorage.setItem("ACCESS_TOKEN", response.token);
-      // token이 존재하는 경우 Todo 화면으로 리디렉트
-      window.location.href = "/";
-    }
-  });
+  return call("/auth/signin", "POST", userDTO)
+    .then((response) => {
+      console.log(response); // 응답 객체 확인
+      if (response && response.token) {
+        localStorage.setItem("ACCESS_TOKEN", response.token);
+        window.location.href = "/";
+      } else {
+        console.error("응답에 토큰이 없습니다.");
+      }
+    })
+    .catch((error) => {
+      console.error("로그인 요청 실패:", error);
+    });
 }
 
 export function signout() {
